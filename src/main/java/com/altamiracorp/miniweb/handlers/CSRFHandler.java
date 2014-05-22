9fd3bb09702c9464c906f1fcef1fb8dcc20b7070
Jeff Kunkle <jeff.kunkle@altamiracorp.com>
2014-05-22 09:46:36 -0400

    Add handler to protect against CSRF attacks

---
+++ src/main/java/com/altamiracorp/miniweb/handlers/CSRFHandler.java
@@ -0,0 +1,94 @@
+package com.altamiracorp.miniweb.handlers;
+
+import com.altamiracorp.miniweb.Handler;
+import com.altamiracorp.miniweb.HandlerChain;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+public class CSRFHandler implements Handler {
+    public static final String CSRF_TOKEN_ATTR = "miniweb.csrf.token";
+
+    private final String tokenRequestParameterName;
+
+    public static String getSavedToken(HttpServletRequest request) {
+        return getSavedToken(request, false);
+    }
+
+    public static String getSavedToken(HttpServletRequest request, boolean createTokenIfMissing) {
+        HttpSession session = request.getSession(createTokenIfMissing);
+        if (session == null) {
+            return null;
+        }
+
+        String token = (String) session.getAttribute(CSRF_TOKEN_ATTR);
+        if (token == null && createTokenIfMissing) {
+            token = resetSavedToken(request);
+        }
+
+        return token;
+    }
+
+    private static String resetSavedToken(HttpServletRequest request) {
+        String token = new BigInteger(120, new SecureRandom()).toString(32);
+        request.getSession().setAttribute(CSRF_TOKEN_ATTR, token);
+        return token;
+    }
+
+    private static void clearSavedToken(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            request.getSession().removeAttribute(CSRF_TOKEN_ATTR);
+        }
+    }
+
+    public CSRFHandler(String tokenRequestParameterName) {
+        this.tokenRequestParameterName = tokenRequestParameterName;
+    }
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, HandlerChain chain) throws Exception {
+        if (request.getMethod().equals("GET")) {
+            verifyTokenAbsence(request);
+        } else {
+            verifyToken(request, response);
+        }
+        chain.next(request, response);
+    }
+
+    private void verifyToken(HttpServletRequest request, HttpServletResponse response) throws TokenException {
+        String tokenFromRequest = getTokenFromRequest(request);
+        String storedToken = CSRFHandler.getSavedToken(request);
+        if (tokenFromRequest == null) {
+            throw new TokenException("CSRF token not found in request parameter " + this.tokenRequestParameterName);
+        }
+        if (storedToken == null) {
+            throw new TokenException("CSRF token has not been set in the user's session");
+        }
+        if (!tokenFromRequest.equals(storedToken)) {
+            throw new TokenException("CSRF token from request parameter " + this.tokenRequestParameterName
+                    + " does not match the one stored in the user's session");
+        }
+    }
+
+    private void verifyTokenAbsence(HttpServletRequest request) throws TokenException {
+        String tokenFromRequest = getTokenFromRequest(request);
+        if (tokenFromRequest != null) {
+            CSRFHandler.resetSavedToken(request);
+            throw new TokenException("CSRF token found in a " + request.getMethod() + " request. Token has been reset");
+        }
+    }
+
+    private String getTokenFromRequest(HttpServletRequest request) {
+        return request.getParameter(this.tokenRequestParameterName);
+    }
+
+    public class TokenException extends Exception {
+        public TokenException(String msg) {
+            super(msg);
+        }
+    }
+}
